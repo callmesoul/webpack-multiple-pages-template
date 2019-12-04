54c0ce16ae31a229983f731eb8866a086792fa85
callmesoul <547437716@qq.com>
2019-12-04 15:01:41 +0800

    完善htmldemo

--- build/until.js
+++ build/until.js
@@ -1,48 +1,9 @@
 var glob = require('glob');
 
-function getEntry() {
-    var entry = {};
-    //读取src目录所有page入口
-    glob.sync('./src/js/**/*.js').forEach(function (name) {
-        var start = name.indexOf('src/') + 4;
-        var end = name.length - 3;
-        var eArr = [];
-        var n = name.slice(start, end);
-        n = n.split('/')[1];
-        eArr.push(name);
-        eArr.push('babel-polyfill'); //引入这个，是为了用async await，一些IE不支持的属性能够受支持，兼容IE浏览器用的
-        entry[n] = eArr;
-    })
-    return entry;
-}
-
-function getHtmlConfig(name, chunks) {
-    return {
-        template: `./src/pages/${name}.html`,
-        filename: `pages/${name}.html`,
-        inject: true,
-        hash: false,
-        chunks: [name]
-    }
-}
-
-function getIPAdress() {
-    var interfaces = os.networkInterfaces();
-    for (var devName in interfaces) {
-        var iface = interfaces[devName];
-        for (var i = 0; i < iface.length; i++) {
-            var alias = iface[i];
-            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-                return alias.address;
-            }
-        }
-    }
-}
-
 module.exports = {
     getEntry: function () {
         var entry = {};
-        //读取src目录所有page入口
+        // 读取src目录所有page入口
         glob.sync('./src/js/**/*.js').forEach(function (name) {
             var start = name.indexOf('src/') + 4;
             var end = name.length - 3;
--- public/index.js
+++
@@ -1,6 +0,0 @@
-import './index.scss';
-import './font-awesome.min.css';
-console.log('public');
-
-let a = 10;
-console.log(a);
